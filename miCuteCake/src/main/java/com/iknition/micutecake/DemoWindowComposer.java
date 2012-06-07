a0e7c69ab2eba8637aa86856952b70f23d086e79
kossel <kossel@192.168.1.108>
2012-06-07 09:17:09 -0500

    fixed tomcat bug

---
+++ miCuteCake/src/main/java/com/iknition/micutecake/DemoWindowComposer.java
@@ -0,0 +1,12 @@
+package com.iknition.micutecake;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+public class DemoWindowComposer extends GenericForwardComposer {
+	
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		
+	}
+}
