ed02baaddcb552958d55498ec11daa78ba171460
kossel <kossel@192.168.1.108>
2012-06-12 17:57:48 -0500

    first load without zkscript

--- miCuteCake/src/main/java/com/iknition/micutecake/controller/ProductTypeController.java
+++ miCuteCake/src/main/java/com/iknition/micutecake/controller/ProductTypeController.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
@@ -23,6 +25,7 @@ import org.zkoss.zkplus.spring.SpringUtil;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Window;
 
 /**
  *
@@ -41,63 +44,26 @@ public class ProductTypeController extends SelectorComposer {
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
-      //  binder = (AnnotateDataBinder) page.getAttribute("binder");
-   //     this.productTypeService = ServiceLocator.getBean(ProductTypeService.class);
-     //   System.out.println("insta: "+comp.getFellowIfAny("typeList").getWidgetClass());
-       // loadAll((Listbox)comp.getFellowIfAny("typeList"));
-          System.out.println("listbos "+typeList);
-          System.out.println("servi: "+productTypeService);
-      //  this.typeList=(Listbox)comp.getFellowIfAny("typeList");
-        
         typeList.setModel(new ListModelList(productTypeService.getAll()));
         typeList.setItemRenderer(new ProductTypeListRenderer());
     }
-    
-    
-    public void onCreate(Listbox lb){
-        List lista = productTypeService.getAll();
-        System.out.println("tamano on create "+lista.size());
-        BindingListModelList model = new BindingListModelList(lista, false);
-        lb.setItemRenderer(new ProductTypeListRenderer());
-        lb.setModel(model); 
-    }
-    
-    @Listen("onAfterRender = listbox#typeList")
-    public void bam(){
-        System.out.println("BAMMMM ");
-    }
-    
-    
-    public void loadAll(Listbox lb){
-        System.out.println("servi: "+productTypeService);
-         System.out.println("listbos "+lb);
-         List lista = productTypeService.getAll();
-         System.out.println("tam "+lista.size());
-        BindingListModelList model = new BindingListModelList(lista, false);
-        lb.setItemRenderer(new ProductTypeListRenderer());
-        lb.setModel(model);              
-    }
-    
+
     @Listen("onClick = button#load")
     public void loadAll(){
-        System.out.println("servi: "+productTypeService);
-         System.out.println("listbos "+typeList);
         BindingListModelList model = new BindingListModelList(productTypeService.getAll(), false);
-        
         typeList.setItemRenderer(new ProductTypeListRenderer());
         typeList.setModel(model);              
     }
     
-  //  @Listen("onCreate = listbox#typeList")
-    public void cargar() {
-        System.out.println("servi: "+productTypeService);
-        System.out.println("listbos "+typeList);
-        BindingListModelList model = new BindingListModelList(productTypeService.getAll(), false);
-        
-        typeList.setItemRenderer(new ProductTypeListRenderer());
-        typeList.setModel(model);   
+     @Listen("onClick = #new")
+    public void showModal(Event e) {
+        Component comp = Executions.createComponents(
+                "/new.zul", null, null);
+        if(comp instanceof Window) {
+            ((Window)comp).doModal();
+        }
     }
-    
+
     public Listbox getTypeList() {
         return typeList;
     }
